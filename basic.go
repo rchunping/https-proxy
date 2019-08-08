a2925debdf46c7f09c14719476f1b891c9665672
pubby <ruanchunping@gmail.com>
2019-08-08 10:56:49 +0800

    basic auth

--- basic.go
+++ basic.go
@@ -2,26 +2,45 @@ package main
 
 import (
 	"encoding/base64"
-	"log"
 	"net/http"
 	"strings"
 )
 
-func basicAuth(w http.ResponseWriter, r *http.Request) bool {
+type User struct {
+	UserId   string
+	Password string
+}
+
+func basicAuth(w http.ResponseWriter, r *http.Request, users []User) bool {
+
 	var auth = r.Header.Get("Proxy-Authorization")
+
 	if ms := strings.Split(auth, " "); len(ms) == 2 && ms[0] == "Basic" {
-		// 验证用户
+
+		// check user:password
 		up, err := base64.StdEncoding.DecodeString(ms[1])
-		log.Printf("%s", up)
+
 		if err == nil {
 			if ms := strings.Split(string(up), ":"); len(ms) == 2 {
-				if ms[0] == "test" {
+
+				var user, password = ms[0], ms[1]
+				var ok = false
+
+				for _, u := range users {
+					if u.UserId == user && u.Password == password {
+						ok = true
+						break
+					}
+				}
+
+				if ok {
 					return true
 				}
 			}
 		}
 		w.WriteHeader(http.StatusForbidden)
 	} else {
+
 		w.WriteHeader(http.StatusProxyAuthRequired)
 		w.Header().Set("Proxy-Authenticate", `Basic realm="Http Proxy"`)
 	}
--- main.go
+++ main.go
@@ -5,22 +5,33 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	var pemPath, keyPath, proto, listen string
+	var pemPath, keyPath, proto, listen, users string
 	flag.StringVar(&pemPath, "pem", "server.pem", "path to pem file")
 	flag.StringVar(&keyPath, "key", "server.key", "path to key file")
 	flag.StringVar(&proto, "proto", "http", "Proxy protocol (http or https)")
 	flag.StringVar(&listen, "listen", ":8080", "listen address, default :8080")
+	flag.StringVar(&users, "users", "", "user:password list")
 	flag.Parse()
 	if proto != "http" && proto != "https" {
 		log.Fatal("Protocol must be either http or https")
 	}
+
+	var userList []User
+	for _, up := range strings.Split(users, ";") {
+		if ms := strings.Split(up, ":"); len(ms) == 2 && len(ms[0]) > 0 {
+			userList = append(userList, User{ms[0], ms[1]})
+		}
+	}
+
 	server := &http.Server{
 		Addr: listen,
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if !basicAuth(w, r) {
+
+			if len(userList) > 0 && !basicAuth(w, r, userList) {
 				return
 			}
 
