8105213512503c87e2fbb897071dd39c5b4c0c79
rchunping <ruanchunping@gmail.com>
2019-08-10 11:52:33 +0800

    fix

--- cert.sh
+++ cert.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-case $(uname -s) in
-Linux*) sslConfig=/etc/ssl/openssl.cnf ;;
-Darwin*) sslConfig=/System/Library/OpenSSL/openssl.cnf ;;
-*) sslConfig=/etc/ssl/openssl.cnf ;;
-esac
 
 openssl req \
   -newkey rsa:2048 \
@@ -13,9 +8,14 @@ openssl req \
   -new \
   -out server.pem \
   -subj /CN=localhost \
-  -reqexts SAN \
-  -extensions SAN \
-  -config <(cat $sslConfig \
-    <(printf '[SAN]\nsubjectAltName=DNS:localhost')) \
+  -extensions san \
+  -config \
+  <(echo "[req]";
+    echo distinguished_name=req;
+    echo "[san]";
+    echo subjectAltName=DNS:localhost,IP:10.0.0.1
+   ) \
   -sha256 \
   -days 3650
+
+#openssl req -x509 -newkey rsa:4096 -keyout server.key -out server.pem -days 3650 -nodes -subj '/CN=localhost'
